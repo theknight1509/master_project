9264797d3ca09ee8240be81668afcdbb0050c37e
(/)yvind <o.b.svendsen@astro.uio.no>
2017-10-16 19:21:43 +0200

    Bestfit v1 - vary a single parameter at a time
    Over the next couple of days this folder will be filled with scripts
    and plots that documnet the manipulation of a single parameter at a given time.

---
+++ bestfit_param_omega/find_bestfit_param_v1/README.md
@@ -0,0 +1,9 @@
+Find bestfit parameters for 'Omega'
+===========================================
+
+Version One - Single parameters
+-----------------------------------
+
+This folder is varying a single parameter at a time.
+
+mgal - The initial mass of the galaxy
---
+++ bestfit_param_omega/find_bestfit_param_v1/vary_mgal.py
@@ -0,0 +1,10 @@
+"""
+"""
+M_star_t0_MW = (6.43e10, 0.63e10) #[M_sol] #http://adsabs.harvard.edu/abs/2011MNRAS.414.2446M
+M_gas_t0_MW = 6.7e9 #[M_sol] #https://ay201b.wordpress.com/2013/01/14/chapter-density-of-the-milky-ways-ism/
+M_baryon_t0_MW = (M_star_t0_MW[0]+M_gas_t0_MW, M_star_t0_MW[1]+M_gas_t0_MW)
+
+M_star_t0_MW = (54.3e9, 5.7e9) #[M_sol] #http://adsabs.harvard.edu/abs/2017MNRAS.465...76M
+M_gas_t0_MW = 1.2e9 #[M_sol] #https://ui.adsabs.harvard.edu/#abs/2017MNRAS.465...76M/abstract
+M_baryon_t0_MW = (M_star_t0_MW[0]+M_gas_t0_MW, M_star_t0_MW[1]+M_gas_t0_MW)
+

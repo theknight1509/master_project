67d75e681ed237ff526825cf92e012fb19ed9236
oyvinbsv <o.b.svendsen@astro.uio.no>
2017-11-27 19:01:12 +0100

    Test and debug analyze_stddev.py on /stornext/

--- manip_yields_experiment/analyze_stddev.py
+++ manip_yields_experiment/analyze_stddev.py
@@ -6,28 +6,33 @@ import os, sys
 import numpy as np
 from directory_master import Foldermap
 folder = Foldermap()
-default_filename_postamble = "deafult.npy" #Final string of default-datafile-name
-index_filename = "data_indeces.npy"
+default_filename_postamble = "default.npy" #Final string of default-datafile-name
+index_filename = "data_indeces.txt"
 
 loa_contents = os.listdir('.')
 loa_folders = []
 for content in loa_contents: #loop over all strings in cwd
     if not ('.' in content): #string is not a filename, but folder
         loa_folders.append(content)
-    elif (content[-len(default_filename_postamble):] == default_filename_postamble):
-        #Found default-file (fudgefactor=1) get generic data-file name and default-file name
-        default_filename = content
-        data_filename = content[:-len(default_filename_postamble)]
 
 #find folder name
-print "Current available folders: ", loa_folders
-input_datafolder = raw_input("What folder would you like to choose?")
-if not (input_datafolder in loa_folders):
-    print "Folder: %s not available"%(input_datafolder)
+print "Current available folders: ", dict(enumerate(loa_folders))
+response = raw_input("What folder would you like to choose? (full path or index)")
+try:
+    #is response the index of folder-list?
+    response = int(response)
+    input_datafolder = loa_folders[response]
+except ValueError: 
+    #the response cannot be integer, full path is given
+    input_datafolder = response
+    #check that folder exist in list
+    if not (input_datafolder in loa_folders):
+        print "Folder: %s not available"%(input_datafolder)
+        sys.exit("Exiting!")
+except IndexError:
+    #index is out of range of folder-list
+    print "Index: %d is not available"%response
     sys.exit("Exiting!")
-else:
-    #print "Folder: %s found"%(input_datafolder)
-    None
 
 #choose array
 array_name = "Re-187"
@@ -39,11 +44,20 @@ with open(input_datafolder + '/' + index_filename, 'r') as index_file:
         if line.split()[1] == array_name:
             array_index = line.split()[0]
 
+#find default filename from known postamble
+loa_filenames = os.listdir(input_datafolder)
+for filename in loa_filenames:
+    if (filename[-len(default_filename_postamble):] == default_filename_postamble):
+        #Found default-file (fudgefactor=1) get generic data-file name and default-file name
+        default_filename = filename #full name of datafile with fudgefactor=1
+        data_filename = filename[:-len(default_filename_postamble)] #generic data-path
+        break
+
 #get shape and array from default experiment
 data_matrix_default = np.load(input_datafolder + '/' + default_filename)
 default_shape = data_matrix_default.shape
-num_arrays = default_shape[:,0]
-num_timepoints = default_shape[0,:]
+num_arrays = default_shape[0]
+num_timepoints = default_shape[1]
 default_array = np.copy(data_matrix_default[array_index,:])
 #get number of experiments from README
 readmestring = "Number of experiments:"
@@ -51,20 +65,20 @@ readmefilename = input_datafolder + "/README.md"
 with open(readmefilename, 'r') as readmefile:
     for line in readmefile.readlines():
         if line[:len(readmestring)] == readmestring:
-            num_experiments = int(line[len(readmestring:]))
+            num_experiments = int(line[len(readmestring):])
 
 #Make empty array to fill with all values
-data_matrix_arrays = np.zeros#(num_experiments, num_timepoints) #TODO #collection of arrays wanted
+data_matrix_arrays = np.zeros((num_experiments,num_timepoints)) #collection of arrays wanted
 
 #Go through all data files
 for i in range(num_experiments):
     data_matrix = np.load(input_datafolder + '/' + data_filename + str(i) + '.npy') #collection of all arrays from calculation
-    data_matrix_arrays[i,:] = data_matrix[array_index,:] #TODO #store wanted array from one simulation
+    data_matrix_arrays[i,:] = data_matrix[array_index,:] #store wanted array from one simulation
 
 #get median, mean, and sigma from data_matrix
-data_mean = np.mean(data_matrix_array, axis=0) #TODO
-data_median = np.median(data_matrix_array, axis=0) #TODO
-data_sigma = np.std(data_matrix_array, axis=0) #TODO
+data_mean = np.mean(data_matrix_array, axis=0)
+data_median = np.median(data_matrix_array, axis=0)
+data_sigma = np.std(data_matrix_array, axis=0)
 data_2sigma = 2*data_sigma
 
 #make matrix of all processed arrays

c6084e33c97546299aba187d4bc5a8e1701a7a70
(/)yvind <o.b.svendsen@astro.uio.no>
2017-11-28 15:33:53 +0100

    update shell-script to properly handle directory-file

--- copy_into_stornext.sh
+++ copy_into_stornext.sh
@@ -2,8 +2,9 @@
 #copy files from current working directory into 'stornext'
 stornext_folder="/mn/stornext/d7/oyvinbsv/Master/"
 echo "stornext folder: " $stornext_folder
-loa_files=( "directory_master.py" "bestfit_param_omega/__init__.py" "bestfit_param_omega/bestfit_file.py" "bestfit_param_omega/current_bestfit.py" "bestfit_param_omega/time_sfr_Shen_2015.txt" "manip_yields_experiment/experiment.py" "manip_yields_experiment/paralell_yield_experiment.py" "manip_yields_experiment/make_fudge_factor_table.py" "manip_yields_experiment/analyze_stddev.py")
+loa_files=("bestfit_param_omega/__init__.py" "bestfit_param_omega/bestfit_file.py" "bestfit_param_omega/current_bestfit.py" "bestfit_param_omega/time_sfr_Shen_2015.txt" "manip_yields_experiment/experiment.py" "manip_yields_experiment/paralell_yield_experiment.py" "manip_yields_experiment/make_fudge_factor_table.py" "manip_yields_experiment/analyze_stddev.py")
 loa_dirs=("bestfit_param_omega" "manip_yields_experiment")
+dir_file="directory_master.py"
 
 echo "Are you currently in home-folder on /hume? y/n"
 echo "(usage: y for yes, n for no, f for yes, but new folders are required)"
@@ -31,10 +32,11 @@ do
     cp ${file} ${stornext_folder}${file}
 done
 
-#change directory, and reinitialize directory_master
-echo "Would you like to re-initialize 'directory_master.py'? y/n"
+#copy 'directory_master.py' into /stornext/ and reinitialize if necessary
+echo "Would you like to copy 'directory_master.py'?"
 read response
 if [ $response = "y" ]; then
+    cp ${dir_file} ${stornext_folder}${dir_file}
     echo "Swapping to 'stornext'"
     cd ${stornext_folder}
     echo "Reinitializing 'directory_master.py'"

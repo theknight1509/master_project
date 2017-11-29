c72c7dc6d66fb72cd7d2dd86f98673a0b2d7624e
oyvinbsv <o.b.svendsen@astro.uio.no>
2017-11-29 12:37:41 +0100

    started new plotting-script for plotting some of the results

---
+++ results/plot_stddev.py
@@ -0,0 +1,9 @@
+"""
+TODO LIST:
+[] - write description
+[] - write commented skeleton
+[] - write skeleton from comments
+[] - fill in skeleton with code
+[] - run/test/debug
+[] - rewrite description and write usage
+"""

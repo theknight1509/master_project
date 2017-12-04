805fe3f04906a2f9b0e18e4eecf7f8713e48a177
(/)yvind <o.b.svendsen@astro.uio.no>
2017-12-04 20:00:19 +0100

    seperated plotting into different functions for plotting calculation-time, along with and separate from chi^2 values

--- bestfit_param_omega/resolution_analysis/plot_timesteps.py
+++ bestfit_param_omega/resolution_analysis/plot_timesteps.py
@@ -2,12 +2,19 @@
 Plot the timestep data-tables
 """
 #import matplotlib.pyplot as pl
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as pl
 import os, sys
 
 # get list of tables
-loa_filenames = ["resolutiontable_constant.dat", "resolutiontable_special.dat"]
-response = raw_input("The data files are: %s \n is this okay? (y/n)\t"%loa_filenames)
+loa_filetypes = ["constant", "special"]
+filename = lambda filetype: "resolutiontable_%s.dat"%filetype
+loa_filenames = [filename(filetype) for filetype in loa_filetypes]
+#get response from user
+if len(sys.argv) == 2:
+    response = sys.argv[1]
+else:
+    response = raw_input("The data files are: %s \n is this okay? (y/n)\t"%loa_filenames)
+#act on response
 if response == "y":
     pass
 elif response == "n":
@@ -19,10 +26,10 @@ else:
 
 #Extract data from filenames
 doa_data = {}
-for filename in loa_filenames:
+for filetype in loa_filetypes:
     current_data_dict = {}
     #read from file
-    with open(filename, 'r') as infile:
+    with open(filename(filetype), 'r') as infile:
         title_row = infile.readline().split()
         loa_rows = infile.readlines()
     #make empty list for each title
@@ -37,47 +44,127 @@ for filename in loa_filenames:
             else:
                 current_val = float(words[i])
             current_data_dict[title].append(current_val)
-    doa_data[filename] = current_data_dict
+    doa_data[filetype] = current_data_dict
 #Done extracting data into two nested dictionaries
 
-#plot into three figures, one for each chi^2 method
-
-fig_type = "pearson_chi2" 
-fig_type = "astro_chi2" 
-fig_type = "relative_chi2"
-
-"""
-for i in range(len(fig_name_list)):
-    fig = pl.figure(fig_name_list[i])
-    
-    constant_axis = fig.add_subplot(111)
-    plotting_function(constant_axis, constant_dt_values,
-    constant_value_list[i], fig_str_list[i],
-    constant=True)
-    #make a new axis, inverted, on top
-    special_axis = constant_axis.twiny()
-    special_axis.invert_xaxis()
-    special_axis.set_xscale('log')
-    special_axis.xaxis.tick_top()
-    plotting_function(special_axis, special_dt_values,
-    special_value_list[i], fig_str_list[i],
-    constant=False)
-    
-    axis = fig.add_subplot(111)
+#make function for plottong two different plots in the same eaxis-object
+def plot_resolution(axis, loa_x_axis, loa_y_axis, loa_labels,
+                    color='r', loa_line=["s", "*"], eris=False):
     axis.grid(True)
-    axis.set_title("Resolution of 'Omega' compared to 'Eris'")
-    axis.set_xlabel("number of timepoints")
-    axis.set_ylabel(fig_str_list[i])
-    axis.set_xscale('log')
-    plotting_function(axis, constant_n_values,
-                      constant_value_list[i],
-                      constant=True)
-    plotting_function(axis, special_n_values,
-                      special_value_list[i],
-                      constant=False)
-    axis.legend(numpoints=1, loc='best')
-    #save figures
-    figname = "timestep_resolution_" + fig_name_list[i] + ".png"
-    fig.savefig(figname)
+    axis.hold(True)
+    #plot all arrays in order
+    for index, label in enumerate(loa_labels):
+        axis.plot(loa_x_axis[index], loa_y_axis[index], loa_line[index], color=color, label=label)
+        
+    if eris:
+        #add 'Eris' vertical line
+        eris_num_timepoints = 1001
+        axis.axvline(x=eris_num_timepoints, color='k', label="Eris")
+
+def plot_2in1_filetype(fig_type):
+    """
+    For a given *fig_type*; make an Figure-object and plot chi^2 against timesteps on one axis,
+    flip the axis and plot calc-time against timesteps on the other axis.
+    """
+    toilet = "Resolution of 'Omega' compared to 'Eris'"
+    fig, ax = pl.subplots()
+    ax.set_title(toilet)
+    
+    #plot chi^2 against number of timesteps
+    key_timesteps = "time-points"
+    loa_timepoints = [doa_data[filetype][key_timesteps] for filetype in loa_filetypes]
+    
+    loa_chi2 = [doa_data[filetype][fig_type] for filetype in loa_filetypes]
+    loa_labels = [filetype + " timesteps" for filetype in loa_filetypes]
+    y_limits = [10**3, 10**6]
+    plot_resolution(ax, loa_timepoints, loa_chi2, loa_labels, color='r', eris=True)
+    ax.tick_params(axis='y', colors='r')
+    ax.spines['left'].set_color('r')
+    ax.yaxis.label.set_color('r')
+    ax.legend(numpoints=1)
+    ax.set_xlabel(key_timesteps)
+    ax.set_xscale('log')
+    ax.set_ylabel(fig_type)
+    ax.set_yscale("log")
+    #ax.set_ylim(y_limits)
+
+    #plot calculation time against number of timesteps
+    ax2 = ax.twinx()
+    ax2.yaxis.tick_right()
+    ax2.yaxis.set_label_position("right")
+    key_calctime = "calculation-time"
+    loa_calctime = [doa_data[filetype][key_calctime] for filetype in loa_filetypes]
+    plot_resolution(ax2, loa_timepoints, loa_calctime, loa_labels, color='b', eris=False)
+    ax2.tick_params(axis='y', colors='b')
+    ax2.spines['right'].set_color('b')
+    ax2.yaxis.label.set_color('b')
+    ax2.set_ylabel(key_calctime + " [s]")
+    ax.set_yscale("log")
+    fig.savefig("doubleyaxis_%s_calctime.png"%fig_type)
+
+def plot_filetype(filetype):
+    """
+    For a given *fig_type*; make an Figure-object and plot chi^2 against timesteps.
+    """
+    toilet = "Resolution of 'Omega' compared to 'Eris'"
+    fig, ax = pl.subplots()
+    ax.set_title(toilet)
+    
+    #plot chi^2 against number of timesteps
+    key_timesteps = "time-points"
+    loa_timepoints = [doa_data[filetype][key_timesteps] for filetype in loa_filetypes]
+    
+    loa_chi2 = [doa_data[filetype][fig_type] for filetype in loa_filetypes]
+    loa_labels = [filetype + " timesteps" for filetype in loa_filetypes]
+    y_limits = [10**3, 10**6]
+    
+    plot_resolution(ax, loa_timepoints, loa_chi2, loa_labels, color=None, eris=True)
+
+    ax.legend(numpoints=1)
+    ax.set_xlabel(key_timesteps)
+    ax.set_xscale('log')
+    ax.set_ylabel(fig_type)
+    ax.set_yscale("log")
+    #ax.set_ylim(y_limits)
+
+    fig.savefig("singleyaxis_%s.png"%fig_type)
+
+def plot_calctime():
+    """
+    For a given *fig_type*; make an Figure-object and plot chi^2 against timesteps on one axis,
+    flip the axis and plot calc-time against timesteps on the other axis.
+    """
+    toilet = "Resolution of 'Omega' compared to 'Eris'"
+    fig, ax = pl.subplots()
+    ax.set_title(toilet)
+    
+    #plot chi^2 against number of timesteps
+    loa_labels = [filetype + " timesteps" for filetype in loa_filetypes]
+    y_limits = [10**3, 10**6]
+    
+    key_timesteps = "time-points"
+    loa_timepoints = [doa_data[filetype][key_timesteps] for filetype in loa_filetypes]
+
+    key_calctime = "calculation-time"
+    loa_calctime = [doa_data[filetype][key_calctime] for filetype in loa_filetypes]
+    plot_resolution(ax, loa_timepoints, loa_calctime, loa_labels, color=None, eris=False)
+    
+    ax.set_yscale("log")
+    ax.legend(numpoints=1)
+    ax.set_xlabel(key_timesteps)
+    ax.set_xscale('log')
+    ax.set_ylabel(key_calctime + " [s]")
+    ax.set_yscale("log")
+    fig.savefig("singleyaxis_calctime.png")
+
+if True:
+    ### plot into three figures, one for each chi^2 method ###
+
+    loa_fig_types = ["pearson_chi2", "astro_chi2", "relative_chi2"]
+    for fig_type in loa_fig_types:
+        plot_filetype(filetype=filetype)
+        plot_2in1_filetype(fig_type=fig_type)
+        #pass
+    plot_calctime()
+
 pl.show()
-"""

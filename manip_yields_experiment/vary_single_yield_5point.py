4ece373bddd4045af828c9ff0ffa1f90fb6df095
(/)yvind <o.b.svendsen@astro.uio.no>
2018-02-26 13:25:57 +0100

    tweaking/updating vary-single isotope yields

--- manip_yields_experiment/vary_single_yield_5point.py
+++ manip_yields_experiment/vary_single_yield_5point.py
@@ -6,47 +6,59 @@ Plot [Eu/H] for the all experiments.
 
 ### Import ###
 import sys, os
-import bestfit_param_omega.current_bestfit as current_bestfit
+import bestfit_param_omega.current_bestfit as cbf
 from experiment import experiment
 from visualize import visualize
 
-### set global values ###
-isotope = "Re-187"
-mean_ff = 1.0
-stddev_ff = 0.15
-rel_stddev_ff = mean_ff + stddev_ff
-experiment_timesteps = 500
+### Global values for controlling experiment ###
+global_special_timestep = 0
+global_constant_timestep = cbf.bestfit_dt
 
-### make all experiments ###
-loa_multiples_sigma = [3, 1, 0, -1, -3]
-loa_fudge_factors = [mean_ff + multiple*stddev_ff for multiple in loa_multiples_sigma]
-print loa_fudge_factors
-#Perform experiments in comprehensive list
-loa_experiments = [experiment(isotope, fudge_factor,
-                              input_timesteps=experiment_timesteps,
-                              bestfit_namespace=current_bestfit)
+### Functions for running experiment ###
+def get_relative_yields(filename):
+    """
+    Read 'filename' with isotope, standard value, min and max values.
+    Return a dictionary with isotope-keys and a list of 0, +/- sigma, 
+    and +/- two sigma.
+    """
+    doa_iso_yields = {}
+    with open(filename, 'r') as infile:
+        loa_datarows = infile.readlines() #get list of rows
+        loa_datarows.pop(0) #remove header
+        for row in loa_datarows:
+            col_values = row.split(' ') #split into words
+            isotope = col_values.pop(0) #remove iso-string
+            col_values = [float(col_value) for col_value in col_values]
+            rel_diff_with_index0 = [(val - col_values[0])/col_values[0]
+                                    for val in col_values]
+            #make list of relative yield uncertainties
+            loa_rel_diff = [2*min(rel_diff_with_index0),
+                            min(rel_diff_with_index0),
+                            0, max(rel_diff_with_index0),
+                            2*max(rel_diff_with_index0)]
+            doa_iso_yields[isotope] = loa_rel_diff
+    return doa_iso_yields
+
+def experiment_with_fudge_factors(isotope, loa_fudge_factors):
+    #Perform experiments in comprehensive list
+    loa_experiments = [experiment(isotope, fudge_factor,
+                                  input_timesteps=global_special_timestep,
+                                  dt=global_constant_timestep
+                                  bestfit_namespace=cbf)
                    for fudge_factor in loa_fudge_factors]
-#list of names of experiments
-loa_names = [r"$\hat{Y}_{%s}=%1.2f\sigma$"%(isotope, sigma)
-             for sigma in loa_multiples_sigma]
+    #list of names of experiments
+    loa_names = [r"$\hat{Y}_{%s}=%1.2f\sigma$"%(isotope, sigma)
+                 for sigma in loa_multiples_sigma]
 
-### plot with visualize ###
-title = "Spectroscopic abundance of %s with different yield-output"%(isotope)
-filename = "yield_%s_5point.png"%(isotope)
-#plotting object
-vis_object = visualize(loa_experiments, loa_names, num_yaxes=2, yields=True)
-#plot spectroscopic abundance
-vis_object.add_time_relabu_omegaonly(isotope)
-vis_object.add_yields(nuclide=isotope, index_yaxis=1, time="sum", log_bool=False)
-vis_object.finalize(show=False, save=filename,
-                    title=title, linewidth=3)
+    #save figure
+    title = "%s abundance in proxy-MW by 'Omega'"%(isotope)
+    filename = "ism_%s_5point.png"%(isotope)
+    #plot ism-content for isotope in question.
+    vis_object = visualize(loa_experiments, loa_names, num_yaxes=2)
+    #plot spectroscopic abundance
+    vis_object.add_time_ism(isotope, index_yaxis_0)
+    vis_object.finalize(show=True, save=filename,
+                        title=title, linewidth=3)
+    vis_object.add_yields(nuclide=isotope, index_yaxis=1, time="sum", log_bool=False)
 
-title = "%s abundance in proxy-MW by 'Omega'"%(isotope)
-filename = "ism_%s_5point.png"%(isotope)
-#plot ism-content for isotope in question.
-vis_object = visualize(loa_experiments, loa_names, num_yaxes=2)
-#plot spectroscopic abundance
-vis_object.add_time_ism(isotope)
-vis_object.add_eris_sfr(index_yaxis=1)
-vis_object.finalize(show=True, save=filename,
-                    title=title, linewidth=3)
+

30ab2c5620271ad21c11424ce59613377578b27f
(/)yvind <o.b.svendsen@astro.uio.no>
2018-04-22 17:05:12 +0200

    plotting scripts for probing MCsim results

--- manip_multiple_param/analyze/plot_data_files.py
+++ manip_multiple_param/analyze/plot_data_files.py
@@ -15,7 +15,8 @@ def get_mean_sigma_extrema(numpy_matrix):
     minima = np.amin(numpy_matrix, axis=axis)
     return mean, pos_sigma, neg_sigma, maxima, minima
 
-def plot_mean_sigma_extrema(time_array, toa_arrays, toa_labels=False):
+def plot_mean_sigma_extrema(time_array, toa_arrays,
+                            toa_labels=False):
     fig = pl.figure()
     ax = fig.gca(); ax.grid(True)
     for array in toa_arrays:
@@ -24,17 +25,22 @@ def plot_mean_sigma_extrema(time_array, toa_arrays, toa_labels=False):
         ax.legend(toa_labels, numpoints=1, loc='lower right')
     return fig
 
-### On direct call! ###
-if __name__ == '__main__':
-    dir_name = "../test_dir/"
+def plot_all_mean_sigma_extrema(dir_name, loa_array_strings):
     time_array = get_single_array(dir_name, array_string="time")
-    loa_array_strings = ["num_nsm", "ism_iso_Re-187", "ism_iso_Os-187"]
     doa_arrays = get_all_arrays(dir_name, loa_array_strings)
+    
     doa_stats_arrays = {}
+    doa_figs = {}
     for key in loa_array_strings:
         numpy_mat = doa_arrays[key]
         doa_stats_arrays[key] = get_mean_sigma_extrema(numpy_mat)
-    key = loa_array_strings[1]
-    fig = plot_mean_sigma_extrema(time_array,doa_stats_arrays[key])
-    fig.show()
-    raw_input("Continue to next plot")
+        fig = plot_mean_sigma_extrema(time_array,doa_stats_arrays[key])
+        doa_figs[key] = fig
+
+    return loa_figs
+
+### On direct call! ###
+if __name__ == '__main__':
+    dir_name = "../test_dir/"
+    loa_array_strings = ["num_nsm", "ism_iso_Re-187", "ism_iso_Os-187"]
+    plot_all_mean_sigma_extrema(dir_name, loa_array_strings)
--- manip_multiple_param/analyze/plot_save_stornext_data.py
+++ manip_multiple_param/analyze/plot_save_stornext_data.py
@@ -4,8 +4,32 @@ with mean + regions and such.
 Decide which data is to be stored in /results/
 """
 from directory_master import Foldermap
+from plot_data_files import plot_all_mean_sigma_extrema
 
 #Get relevant directory-names for uio-systems
 folder_instance = Foldermap()
 dir_stornext = folder_instance.stornext_folder()
 dir_hume = folder_instance.hume_folder()
+
+#decide on variables to plot!
+loa_elem = ["Re", "Os"]
+loa_re_isos = ["Re-187", "Re-185"]
+loa_os_isos = ["Os-187", "Os-188"]
+loa_ism_isos = ["ism_iso_"+iso for iso
+                in loa_re_isos+loa_os_isos]
+loa_ism_elem = ["ism_elem_"+elem for elem in loa_elem]
+loa_yield_isos = ["yield_iso_"+iso for iso
+                  in loa_re_isos+loa_os_isos]
+loa_yield_elem = ["yield_elem_"+elem for elem in loa_elem]
+loa_array_strings = ["num_nsm", "m_locked"] + \
+                    loa_ism_isos + loa_ism_elem + \
+                    loa_yield_isos + loa_yield_elem
+
+if __name__ == '__main__':
+    print "Plotting data for the following arrays:"
+    print loa_array_strings
+
+    dir_experiment = dir_stornext + "MCExperiment1/"
+
+    loa_figs = plot_all_mean_sigma_extrema(dir_experiment,
+                                           loa_array_strings)

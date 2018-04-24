5216fe8726caee73c5c1c59bef5b7acc37a5103a
(/)yvind <o.b.svendsen@astro.uio.no>
2018-04-24 09:59:16 +0200

    script for saving correct results to correct folder

---
+++ manip_multiple_param/analyze/save_results.py
@@ -0,0 +1,24 @@
+"""
+"""
+
+def save_results(directory, experiment, array_string):
+    """ Some DocString """
+    all_good = False
+    #create outfilename dir+resultfile+exp+arrstr+type
+    #get all matrices
+    #get time evolution arrays
+    ###get arrays
+    ###get filename type=timeevol.npy
+    ###get desc. string of all arrays
+    #get histogram array
+    ###get arrays
+    ###get filename type=hist.npy
+    ###get desc. string of all arrays
+    #get description
+    ###describe content
+    ###get filename=desc.txt
+    #Save all files
+    if all_good:
+        return True
+    else:
+        return False

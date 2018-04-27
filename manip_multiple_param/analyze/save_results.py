51189bb72c8097a740c9564cc1ce0b2406394cd3
(/)yvind <o.b.svendsen@astro.uio.no>
2018-04-27 12:59:57 +0200

    working on saving appropriate results

--- manip_multiple_param/analyze/save_results.py
+++ manip_multiple_param/analyze/save_results.py
@@ -1,22 +1,30 @@
 """
 """
+from handle_data_files import get_all_arrays
 
-def save_results(directory, experiment, array_string):
+def save_results(directory, experiment, loa_array_strings):
     """ Some DocString """
     all_good = False
-    #create outfilename dir+resultfile+exp+arrstr+type
     #get all matrices
-    #get time evolution arrays
-    ###get arrays
-    ###get filename type=timeevol.npy
-    ###get desc. string of all arrays
-    #get histogram array
-    ###get arrays
-    ###get filename type=hist.npy
-    ###get desc. string of all arrays
-    #get description
-    ###describe content
-    ###get filename=desc.txt
+    doa_2Darrays = get_all_arrays(dir_name=directory, loa_array_strings=loa_array_strings)
+    time_array = get_single_array(dir_name=directory, array_string="time")
+    for array_string in loa_array_strings:
+        #create outfilename dir+resultfile+exp+arrstr+type
+        outfilename = lambda string: directory + "resultfile_" + \
+                      experiment + "_" + \
+                      array_string + "_" + string
+        #get time evolution arrays
+        
+        ###get arrays
+        ###get filename type=timeevol.npy
+        ###get desc. string of all arrays
+        #get histogram array
+        ###get arrays
+        ###get filename type=hist.npy
+        ###get desc. string of all arrays
+        #get description
+        ###describe content
+        ###get filename=desc.txt
     #Save all files
     if all_good:
         return True

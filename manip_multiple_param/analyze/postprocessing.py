9cc8f05c2b20389ae86ee103e3211cc28cd01840
oyvinbsv <o.b.svendsen@astro.uio.no>
2018-05-07 19:02:04 +0200

    postprocessing now works on beehive

--- manip_multiple_param/analyze/postprocessing.py
+++ manip_multiple_param/analyze/postprocessing.py
@@ -3,15 +3,16 @@
 Class and function for extracting data from 
 sea of data-files in experiment-folders.
 """
+import os
+import numpy as np
 
 class Extract(object):
     def __init__(self, dir_name):
         self.dir_string_func = lambda dir_name: dir_name if (dir_name[-1]=="/") else dir_name + "/"
-        self.data_index_filename = dir_string_func(dir_name) + "data_indeces.txt"
+        self.data_index_filename = self.dir_string_func(dir_name) + "data_indeces.txt"
 
         self.get_dir = dir_name
         self.save_dir = dir_name
-        return
 
     def __call__(self):
         self.setup_extract_reos()
@@ -31,7 +32,7 @@ class Extract(object):
             #split row into index and array-name
             current_row = content_row.split()
             current_index = int(current_row[0])
-            current_array_name = int(current_row[1])
+            current_array_name = current_row[1]
             if (current_array_name == string):
                 return_index = current_index
                 found_index = True
@@ -57,22 +58,24 @@ class Extract(object):
         'extract_func' - must take numpy matrix and return single array
         """
         loa_extracted_arrays = []
-        for data_filename in get_numpy_filenames(): #loop over numpy-files
+        for data_filename in self.get_numpy_filenames(): #loop over numpy-files
             data = np.load(data_filename) #get data from a single data-file
             extracted_arr = extract_func(data) #extract according to input function
-            loa_extracted_arrays.append(extraced_arr) #store extracted array
-        extacted_data = np.stack(loa_extracted_arrays) #stack into matrix of extracted arrays
+            loa_extracted_arrays.append(extracted_arr) #store extracted array
+        extracted_data = np.stack(loa_extracted_arrays) #stack into matrix of extracted arrays
         extracted_filename = self.dir_string_func(self.save_dir) + "extract_" + extract_filename + ".npy"
-        np.save(extract_filename, extracted_data) #save extracted data
+        np.save(extracted_filename, extracted_data) #save extracted data
+        print "Saving extracted data to %s"%(extracted_filename)
         return
 
     def extract_single_array(self, data, index_array):
-        return data[index,:]
+        return data[index_array,:]
     
     def extract_ratio_array(self, data, index_numer, index_denom):
         numerator = data[index_numer,:]
         denominator = data[index_denom,:]
-        return numerator/denominator
+        return_array = np.nan_to_num(numerator/denominator)
+        return return_array
 
     def get_time_array(self):
         #Get time-array from the first numpy-filename
@@ -81,7 +84,7 @@ class Extract(object):
         numpy_filename = loa_numpy_filenames[0] #choose the first filename
 
         data = np.load(numpy_filename)
-        time_array = single_matrix[index_time,:]
+        time_array = data[index_time,:]
 
         return time_array
 
@@ -96,11 +99,11 @@ class Extract(object):
 
         #extract single-isotope ism-array from loa_keys
         for key in loa_keys:
-            extract_func = lambda data: extract_single_array(data=data, index_array=doa_array_index[key])
+            extract_func = lambda data: self.extract_single_array(data=data, index_array=doa_array_index[key])
             self.handle_all_data(extract_func=extract_func,
                                  extract_filename=doa_filename[key])
         #extract re187/os-187 ism-array
-        extract_func = lambda data: extract_ratio_array(data=data,
+        extract_func = lambda data: self.extract_ratio_array(data=data,
                                                         index_numer=doa_array_index[re187],
                                                         index_denom=doa_array_index[re187])
         self.handle_all_data(extract_func=extract_func,
@@ -108,13 +111,11 @@ class Extract(object):
         return
 
 
-def Reduce(Extract):
+class Reduce(Extract):
     def __init__(self, dir_name):
-        Extract.__init__(self, dir_name)
+        Extract.__init__(self, dir_name=dir_name)
 
-        return
-
-    def __call__():
+    def __call__(self):
         self.setup_reduce_reos()
         return
     
@@ -127,10 +128,10 @@ def Reduce(Extract):
         axis = 0
         time_array = self.get_time_array()
         data = np.load(filename)
-        mean = np.mean(numpy_matrix, axis=axis)
-        sigma = np.std(numpy_matrix, axis=axis)
-        maximum = np.amax(numpy_matrix, axis=axis)
-        minimum = np.amin(numpy_matrix, axis=axis)
+        mean = np.mean(data, axis=axis)
+        sigma = np.std(data, axis=axis)
+        maximum = np.amax(data, axis=axis)
+        minimum = np.amin(data, axis=axis)
         
         doa_arrays = {}
         doa_arrays["time"] = time_array
@@ -153,7 +154,7 @@ def Reduce(Extract):
         for timepoint in loa_timepoints:
             #find exact values
             if timepoint in time_array: 
-                numpy_array = numpy_matrix[:,timepoint==time_array] #get values
+                numpy_array = data[:,timepoint==time_array] #get values
                 numpy_array = numpy_array.reshape(len(numpy_array)) #flatten to 1D
             #interpolate
             elif (timepoint>time_array[0]) and (timepoint<time_array[-1]): 
@@ -167,8 +168,8 @@ def Reduce(Extract):
                 upper_interp_time = time_array[upper_interp_index]
                 fraction_interp_time = (timepoint-lower_interp_time)/(upper_interp_time-lower_interp_time) #relative timejump between time-array and timepoint
                 
-                lower_interp_numpy_array = numpy_matrix[:,lower_interp_index] #values of numpy_matrix closest to timepoint
-                upper_interp_numpy_array = numpy_matrix[:,upper_interp_index]
+                lower_interp_numpy_array = data[:,lower_interp_index] #values of numpy_matrix closest to timepoint
+                upper_interp_numpy_array = data[:,upper_interp_index]
                 delta_interp_numpy_array = (upper_interp_numpy_array-lower_interp_numpy_array)*fraction_interp_time #jump in numpy_matrix to timepoint
                 
                 numpy_array = lower_interp_numpy_array + delta_interp_numpy_array #interpolated array
@@ -176,15 +177,17 @@ def Reduce(Extract):
                 print "Extrapolation not considered! Fuck off!"
                 print "timepoint", timepoint, time_array[0], time_array[-1]
                 
-            doa_arrays["t=%1.2fGyr"%timepoint/1.0e+9] = numpy_array
+            doa_arrays["t=%1.2fGyr"%(float(timepoint)/1.0e+9)] = numpy_array
     
         return doa_arrays
 
     def save_pandas(self, save_filename, dictionary):
         """ Save dictionary as panda-dataframe in csv"""
+        save_filename = self.dir_string_func(self.save_dir) + "reduce_" + save_filename + ".csv"
         import pandas as pd
         pandaframe = pd.DataFrame(dictionary)
-        pandaframe.to_csv(self.dir_string_func(self.save_dir) + "reduce_" + save_filename + ".csv")
+        pandaframe.to_csv(save_filename)
+        print "Saving reduced PandasDataFrame to %s"%(save_filename)
 
         return
     
@@ -210,25 +213,29 @@ def Reduce(Extract):
         for extracted_filename in loa_extracted_filenames:
             #get relevant filename-section
             save_filename = extracted_filename.split("/")[-1]
-            save_filename = save_filename[len("extracted_"):]
-            save_filename = save_filename[:len(".npy")]
-
+            save_filename = save_filename[len("extract_"):]
+            save_filename = save_filename[:-len(".npy")]
+            
             timeevol_dict = self.get_timeevol(extracted_filename)
             self.save_pandas(save_filename + "_timeevol", timeevol_dict)
-            hist_dict = self.get_hist(extracted_filename)
+            hist_dict = self.get_hist(filename=extracted_filename, 
+                                      loa_timepoints=loa_timepoints)
             self.save_pandas(save_filename + "_hist", hist_dict)
 
         return
 
 if __name__ == '__main__':
     import configparser as cp
-    config_filename = "config_beehive_revides.ini"
+    config_filename = "../config_beehive_revised.ini"
     config = cp.ConfigParser()
     config.read(config_filename)
     subdir_name = config["montecarlo parameters"]["directory_name"]
+    print "Mucking around in directory: %s"%(subdir_name)
     
     extract_instance = Extract(dir_name=subdir_name) #make instance of extract-class
     extract_instance() #do the stuff for Re-Os
+    print "Succesfully extracted!"
 
     reduce_instance = Reduce(dir_name=subdir_name) #make instance of reduce-class
     reduce_instance() #do the stuff for Re-Os
+    print "Succesfully reduced!"

5490dd0e7d1f29c0bc911016ef3886a1325dbe15
(/)yvind <o.b.svendsen@astro.uio.no>
2018-05-08 10:23:04 +0200

    making plottting syntax

---
+++ manip_multiple_param/analyze/plot_MCExperiment_copy_pandas.py
@@ -0,0 +1,93 @@
+"""
+Plot files in pandas-csv files, and store them in the some folder.
+"""
+import os
+import matplotlib.pyplot as pl
+import numpy as np
+import pandas as pd
+
+import configparser as cp
+config_filename = "../config_beehive_revised.ini"
+config = cp.ConfigParser()
+config.read(config_filename)
+subdir_name = config["montecarlo parameters"]["directory_name"]
+print "Working in directory: %s"%(subdir_name)
+
+from matplotlib import rcParams
+#print rcParams.keys()
+rcParams[u"lines.linewidth"] = 2.0
+
+def plot_timeevol(filename_timeevol):
+    pandas_data_frame = pd.read_csv(filename_timeevol)
+    time = pandas_data_frame
+    mean = 
+    pos_sigma = 
+    neg_sigma = 
+    maximum =
+    minimum =
+    
+    #make figure
+    fig = pl.figure(); ax = fig.gca(); ax.grid(True)
+
+    #scale time to Gyr
+    time /= 1.0e+9
+    ax.set_xlabel("time [Gyr]")
+    
+    #plot mean, sigmas, extremas and shaded region
+    ax.plot(time, mean, label="<>", color='k')
+    ax.fill_between(time, pos_sigma, neg_sigma, color='g')
+    loa_surrounding_array = [pos_sigma, neg_sigma, maximum, minimum]
+    loa_surrounding_name = [r"+1$\sigma$",r"-1$\sigma$","max","min"]
+    for y,y_name in zip(loa_surrounding_array,loa_surrounding_name):
+        ax.plot(time, y, linestyle='--', color='g', label=y_name)
+    ax.legend(loc="upper left")
+
+    return fig
+
+def plot_hist(filename_hist):
+    pandas_data_frame = pd.read_csv(filename_hist)
+
+    sos_formation =
+    now = 
+    #make figure
+    fig = pl.figure()
+    loa_ax = fig.subplots(nrows=2,ncols=1, sharex=True)
+    for ax, array, label in zip(loa_ax, [sos_formation, now], 
+                                 ["t=9.5Gyr","t=14Gyr"]):
+        ax.grid(True)
+        ax.hist(array, bins=50, label=label)
+        ax.axvline(np.mean(array), color='k', 
+                   label=r"$\langle X \rangle \pm 1 \sigma$")
+        ax.axvline(np.mean(array)-np.std(array), color='k')
+        ax.axvline(np.mean(array)+np.std(array), color='k')
+        ax.legend(loc=1)
+    
+    return fig
+
+def get_full_filenames(experiment_folder):
+    """ Return list of csv-filenames in experiment-folder with full path. """
+    #get list of filenames in experiment-folder
+    file_list = os.listdir(experiment_folder)
+
+    #add '/' to experiment_folder if necessary
+    if experiment_folder[-1] != '/':
+        experiment_folder = experiment_folder + '/'
+    
+    #get list of filenames with desired quantities
+    csv_file_list = [experiment_folder + filename for filename in file_list
+                     if (".csv" in filename)]
+
+    return csv_file_list
+
+
+
+if __name__ == '__main__':
+    loa_fullpaths = get_full_filenames(subdir_name)
+    for fullpath in loa_fullpaths:
+        if "hist" in fullpath:
+            fig = plot_hist(fullpath)
+        elif "timeevol" in fullpath:
+            fig = plot_hist(fullpath)
+        else:
+            print "(/) Error! neither 'hist' not 'timeevol' present"
+        fig.show()
